b3b070e713903eb4a3ed4c2963d2e40119c6c29d
jrc03 <joserdlc2727@gmail.com>
2026-04-06 19:52:43 -0400

    feat(infrastructure): add user entity configuration to enforce unique email index

---
+++ Infrastructure/Persistence/Config/UserConfig.cs
@@ -0,0 +1,29 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Config
+{
+    public class UserConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+            
+            builder.Property(u => u.Email)
+                   .IsRequired()
+                   .HasMaxLength(150);
+
+            builder.HasIndex(u => u.Email)
+                   .IsUnique();
+
+            builder.Property(u => u.FirstName)
+                   .IsRequired()
+                   .HasMaxLength(50);
+                   
+            builder.Property(u => u.LastName)
+                   .IsRequired()
+                   .HasMaxLength(50);
+        }
+    }
+}
