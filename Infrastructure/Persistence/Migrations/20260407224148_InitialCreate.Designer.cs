b16d7abf19b6fef558f79e1114f0d449cca14d8c
jrc03 <joserdlc2727@gmail.com>
2026-04-07 19:06:52 -0400

    chore(db): add initial EF Core database migration with complete schema constraints

--- Infrastructure/Persistence/Migrations/20260407224148_InitialCreate.Designer.cs
+++ Infrastructure/Persistence/Migrations/20260407224148_InitialCreate.Designer.cs
@@ -4,16 +4,19 @@ using Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260407224148_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
