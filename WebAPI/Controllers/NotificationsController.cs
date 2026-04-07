cb93ebced4c4bf77db58a3fc7d9e9ce74c38cdfb
jrc03 <joserdlc2727@gmail.com>
2026-04-07 17:12:47 -0400

    refactor(notifications): read user id from auth token for unread notifications

--- WebAPI/Controllers/NotificationsController.cs
+++ WebAPI/Controllers/NotificationsController.cs
@@ -1,25 +1,31 @@
 using Application.Features.Notifications.Commands.MarkNotificationAsRead;
 using Application.Features.Notifications.Queries.GetUnreadNotificationsByUser;
+using Application.Interfaces.Auth;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class NotificationsController : ControllerBase
 {
     private readonly ISender _sender;
+    private readonly ICurrentUserService _currentUserService;
 
-    public NotificationsController(ISender sender)
+    public NotificationsController(ISender sender, ICurrentUserService currentUserService)
     {
         _sender = sender;
+        _currentUserService = currentUserService;
     }
 
-    // GET: api/notifications/user/{userId}/unread
-    [HttpGet("user/{userId:guid}/unread")]
-    public async Task<IActionResult> GetUnread(Guid userId)
+    // GET: api/notifications/unread
+    [HttpGet("unread")]
+    public async Task<IActionResult> GetUnread()
     {
+        var userId = _currentUserService.UserId ?? throw new UnauthorizedAccessException("Usuario no válido.");
         var result = await _sender.Send(new GetUnreadNotificationsByUserQuery(userId));
         return Ok(result);
     }
