5ff8839259b3cc2ad1e23dc538bf951a6b6913a0
jrc03 <joserdlc2727@gmail.com>
2026-04-19 18:21:40 -0400

    refactor: use primary constructor in ApplicationDbContext

--- Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -3,15 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        { }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Property> Properties { get; set; }
-        public DbSet<Booking> Bookings { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-        public DbSet<Notification> Notifications { get; set; }
+        public  DbSet<User> Users { get; set; }
+        public  DbSet<Property> Properties { get; set; }
+        public  DbSet<Booking> Bookings { get; set; }
+        public  DbSet<Review> Reviews { get; set; }
+        public  DbSet<Notification> Notifications { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
